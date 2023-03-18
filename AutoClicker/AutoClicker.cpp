701e7317b2a963c237844e8b87f8ca0bdc16e763
Link Jon <Link_Jon@hotmail.com>
2023-03-17 23:43:18 -0400

    Added multiclick capability? Hopefully?
    Further revamped the c++ but, can garentee it still needs work.
    
    And sooner or later i will need to make new functions but, i dont think it needs to be divided up yet...
    However i do know, it should be divided between config and usage. So, will do that later.

--- AutoClicker/AutoClicker.cpp
+++ AutoClicker/AutoClicker.cpp
@@ -1,92 +1,160 @@
 #include <windows.h>
 #include <string>
 #include <iostream>
-using namespace std;
-bool clicking;
-int x;
-int y;
 
-static void startclicker()
-{
-	while (true)
-	{
-		if (GetAsyncKeyState(VK_NUMPAD1))
-		{
-			Sleep(100);
+using namespace std;
+
+string input = "string";
+bool clicking = false;
+bool basic = false;
+bool savefile[10];
+int timeoutSeconds = 120;
+int counter = 0;
+int save = -1;
+int x[10];
+int y[10];
+
+
+int main() {
+
+	// Intro
+
+	system("TITLE Mouse Clicker by sandaasu");
+	cout << "Mouse Clicker by sandaasu\n";
+	cout << "======================================================\n";
+	cout << "[ HOTKEYS ]\n\n";
+	cout << "Numpad1 = Save mouse position\n";
+	cout << "NumPad2 = Activate autoclicker on saved position\n";
+	cout << "NumPad3 = STOP\n";
+	cout << "NumPad5 = Text Commands\n";
+	cout << "Escape  = Close\n\n";
+	cout << "======================================================\n\n";
+	cout << "[ LOG ]\n\n";
+
+	// Main Loop
+
+	while (true) {
+		if (GetAsyncKeyState(VK_NUMPAD1)) {
+			save++;
+			if (save > 9) {
+				cout << "Save file at 9; continue from 0?\n {y / n}\n STRING << ";
+				cin >> input;
+				if (input == "y") { save = 0; };
+				if (input == "n") { save = 9; };
+			};
+
 			POINT cursorPos;
 			GetCursorPos(&cursorPos);
-			//x = 0;
-			x = cursorPos.x;
-			//y = 0;
-			y = cursorPos.y;
-			cout << "Mouse X = " << x << endl;
-			cout << "Mouse Y = " << y << endl;
 
-		}
-		if (GetAsyncKeyState(VK_ESCAPE))
-		{
-			cout << "Bye Bye <3 " << endl;
-			Sleep(500);
-			system("exit");
-		}
-		if (GetAsyncKeyState(VK_NUMPAD2))
-		{
-			cout << "Clicking !" << endl;
+			x[save] = cursorPos.x;
+			y[save] = cursorPos.y;
+			savefile[save] = true;
+
+			cout << "Mouse X = " << x[save] << endl;
+			cout << "Mouse Y = " << y[save] << endl;
+			cout << "Saved to slot " << save << endl;
+
 			Sleep(100);
+		}
+
+		if (GetAsyncKeyState(VK_NUMPAD5)) {
+			cout << "What do you want to do?\n['save' || 'timeout']\n STRING <<";
+			cin >> input;
+			if (input == "timeout") {
+				cout << "What do you want the timer to be? (In Seconds):\nINTEGER << ";
+				cin >> timeoutSeconds;
+				if (timeoutSeconds < 1)
+				{
+					cout << "\nTimeout Disabled\n";
+				};
+				//else;
+				{ cout << "Timeout set to " << timeoutSeconds << "Seconds.\n"; };
+			}//end safety timeout setup logic.
+
+			else if (input == "save") {
+				cout << "What do you want to do?\nForce select save slot [0 - 9] (slot1 to slot 10)\nDelete data in slot [-1 - -10 || -100] (slot1 to slot 10 || ALL)\n";
+				cin >> counter;
+
+				if (counter > -1 and counter < 10) {
+					save = counter;
+				}
+				{//else
+					counter = counter * -1;
+					x[counter] = 0;
+					y[counter] = 0;
+				}
+			}
+
+			{cout << "Warning: Unknown input. Don't capitlize?"; };
+
+		} // END Text commands
+
+		if (GetAsyncKeyState(VK_ESCAPE)) {
+			cout << "Bye Bye <3\n";
+			Sleep(500);
+			PostQuitMessage(0); //Requests to close process, debug detail is 0. (AKA no issue.)
+		}
+
+		if (GetAsyncKeyState(VK_NUMPAD2)) {
+			cout << "Clicking !\n";
 			clicking = true;
+			counter = timeoutSeconds * 1000;
+			Sleep(100);
+		}
 
-			while (true)
-			{
+		while (clicking) {
 
-				if (!clicking)
-				{
-					break;
-				}
+			// locking the mouse isnt exactly user safe
+			// until we can garrentee the stop hotkey will work anywhere.
+			// SetCursorPos()
+			if (basic == true) {
+				mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+				mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+			}
 
-				//Sleep(100);
-				SetCursorPos(x, y);
-				mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
-				mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+			{//Else: 
+				for (int iter = 0; iter >= 10; iter++) {
 
-				if (GetAsyncKeyState(VK_NUMPAD3))
-				{
-					clicking = false;
-					cout << "Stopping the clicker.." << endl;
-				}
+					if (savefile[iter] == false or savefile[iter] == NULL) { continue; };
 
-				if (GetAsyncKeyState(VK_ESCAPE))
-				{
-					cout << "Bye Bye <3 " << endl;
-					Sleep(500);
-					system("exit");
+					if (GetAsyncKeyState(VK_NUMPAD3) or GetAsyncKeyState(VK_NUMPAD2)) {
+						cout << "Stopping the clicker..\n";
+						clicking = false;
+						break;
+					}
+
+					if (GetAsyncKeyState(VK_ESCAPE)) {
+						cout << "Bye Bye <3\n";
+						Sleep(500);
+						PostQuitMessage(0);
+					}
+
+					if (0 >= counter and !1 > timeoutSeconds) { cout << "Timed out\n"; clicking = false; break; };
+					//if timeout exists, and the counter is over, break while loop.
+
+					SetCursorPos(x[iter], y[iter]);
+					mouse_event(MOUSEEVENTF_LEFTDOWN, x[iter], y[iter], 0, 0);
+					mouse_event(MOUSEEVENTF_LEFTUP, x[iter], y[iter], 0, 0);
+
+					counter--;
+					Sleep(1);
 				}
 			}
 		}
-
 	}
 }
 
-static void init()
-{
-	clicking = false;
-	system("TITLE Mouse Clicker by sandaasu");
-	cout << "======================================================" << endl;
-	cout << "[ HOTKEYS ]" << endl << endl;
-	cout << "NumPad1 = save mouse position" << endl;
-	cout << "NumPad2 = activate autoclicker on saved position" << endl;
-	cout << "NumPad3 = STOP" << endl;
-	cout << "Escape  = Close" << endl << endl << endl;
-	cout << " " << endl << endl;
-	cout << "======================================================" << endl;
-	cout << " " << endl << endl;
-	cout << "[ LOG ] " << endl << endl;
-	startclicker();
-}
+//Variant 1
+// A -- Take the user's mouse, and click at specified locations.
+// B -- Extra Credit -- Quit after specified time to assist in damage control.
 
+//Variant 2
+// A -- Find top layer window at coordinate (Find it's HWND, specifically.)
+// B -- Use the HWND to send a WM_LBUTTON at the requested coords
+// C -- Profit.
 
-
-void main()
-{
-	init();
-}
-
+//Variant 3
+// A -- Make a fake mouse
+// B -- Click locations
+// C -- Profit
+// D -- Extra credit; make an opensource multimouse program lol
